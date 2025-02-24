ab7d34596a0c684581bd70e7b1d250882aac2687
MauryaSamanta <mauryasamanta1508@gmail.com>
2025-02-24 20:49:37 +0530

    Fixed login issue

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path="/" element={<LoginPage/>} />
+            <Route path="/" element={!isAuth?<LoginPage/>:<Navigate to="/home" />} />
             
             <Route
               path="/home"
