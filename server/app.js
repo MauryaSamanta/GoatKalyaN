bd00e4803cf8361565c9288d4acf9c43617bfc68
MauryaSamanta <mauryasamanta1508@gmail.com>
2025-02-25 21:31:15 +0530

    helmet issue

--- server/app.js
+++ server/app.js
@@ -21,8 +21,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 dotenv.config();
 app.use(cors());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
 app.use(morgan("commons"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
